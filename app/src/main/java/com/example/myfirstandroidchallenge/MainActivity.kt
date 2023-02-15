bd83ee2535fa079fe24ad19a19e43a7edc1e392e
sanni prasad <sanni.prasad@zestmoney.in>
2023-02-15 17:10:30 +0530

    Added Bottom navigation Moved Recycler view into the first fragment

--- app/src/main/java/com/example/myfirstandroidchallenge/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidchallenge/MainActivity.kt
@@ -3,30 +3,31 @@ package com.example.myfirstandroidchallenge
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myfirstandroidchallenge.models.Product
 import com.example.myfirstandroidchallenge.views.ProductListAdaptor
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
-var products = listOf<Product>(
-    Product("Title 1", "Subtitle 1"),
-    Product("Title 2", "Subtitle 2"),
-    Product("Title 3", "Subtitle 3"),
-)
-
-
+        initNavigation()
     }
-    private fun init() {
-        val rlProducts = findViewById<RecyclerView>(R.id.rvProductList);
-        rlProducts.layoutManager = LinearLayoutManager(this)
-        rlProducts.adapter = ProductListAdaptor(this)
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView: BottomNavigationView =
+            findViewById<View>(R.id.bottomNavigationView) as BottomNavigationView
+        setupWithNavController(bottomNavigationView, navController)
     }
 
 
