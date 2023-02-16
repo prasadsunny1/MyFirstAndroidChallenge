acb60af0e4fa2e7ba28dc4ce1593cb5406797cfd
sanni prasad <sanni.prasad@zestmoney.in>
2023-02-17 02:50:51 +0530

    Integrate room db and build cache strategy

---
+++ app/src/main/java/com/example/myfirstandroidchallenge/database/ProductEntity.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstandroidchallenge.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product_table")
+data class ProductEntity(
+    @PrimaryKey val id: Int,
+    @ColumnInfo val name: String,
+    @ColumnInfo val price: String,
+    @ColumnInfo val extra: String,
+    @ColumnInfo val image: String,
+    @ColumnInfo val timestamp: Long = System.currentTimeMillis()
+)
