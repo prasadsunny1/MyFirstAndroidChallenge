44ae0bab80a856b95215ea416f8c15c1246fedfe
sanni prasad <sanni.prasad@zestmoney.in>
2023-02-23 14:22:44 +0530

    Updated live data testing helper

--- app/src/test/java/com/example/myfirstandroidchallenge/LiveDataTestingHelper.kt
+++ app/src/test/java/com/example/myfirstandroidchallenge/LiveDataTestingHelper.kt
@@ -6,35 +6,26 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
-//class LiveDataTestingHelper {
-//}
-
-/* Copyright 2019 Google LLC.
-   SPDX-License-Identifier: Apache-2.0 */
-
-/// Copied  From an article
 fun <T> LiveData<T>.getOrAwaitValue(
     time: Long = 2,
-    timeUnit: TimeUnit = TimeUnit.SECONDS
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
 ): T {
     var data: T? = null
     val latch = CountDownLatch(1)
     val observer = object : Observer<T> {
-
         override fun onChanged(value: T) {
             data = value
             latch.countDown()
             this@getOrAwaitValue.removeObserver(this)
         }
     }
-
     this.observeForever(observer)
 
     // Don't wait indefinitely if the LiveData is not set.
     if (!latch.await(time, timeUnit)) {
+        this.removeObserver(observer)
         throw TimeoutException("LiveData value was never set.")
     }
 
-    @Suppress("UNCHECKED_CAST")
-    return data as T
+    @Suppress("UNCHECKED_CAST") return data as T
 }
