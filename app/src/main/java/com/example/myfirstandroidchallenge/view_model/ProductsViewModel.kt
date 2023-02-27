a6aba6835c382577ba4857eabf2f7e5107c193e4
sanni prasad <sanni.prasad@zestmoney.in>
2023-02-27 11:28:22 +0530

    Add debouncer

--- app/src/main/java/com/example/myfirstandroidchallenge/view_model/ProductsViewModel.kt
+++ app/src/main/java/com/example/myfirstandroidchallenge/view_model/ProductsViewModel.kt
@@ -1,19 +1,20 @@
 package com.example.myfirstandroidchallenge.view_model
 
+import android.os.Handler
 import androidx.lifecycle.*
 import com.example.myfirstandroidchallenge.repository.ProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
+import java.util.Timer
+import java.util.TimerTask
 import javax.inject.Inject
+import kotlin.concurrent.schedule
 
 @HiltViewModel
 class ProductsViewModel @Inject constructor(
-    private val productRepository: ProductRepository,
-    private val coroutineScope: CoroutineDispatcher
-) :
-    ViewModel() {
-
+    private val productRepository: ProductRepository, private val coroutineScope: CoroutineDispatcher
+) : ViewModel() {
 
     private val _productLoadStates = MutableLiveData<ProductLoadStates>()
 
@@ -21,6 +22,16 @@ class ProductsViewModel @Inject constructor(
      * A state holder for loading, loaded, error and empty states
      */
     val productLoadStates: LiveData<ProductLoadStates> = _productLoadStates
+
+    /**
+     * A timer to delay search operation
+     */
+    private val searchTimer = Timer()
+
+    /**
+     * A timer task to schedule search operation
+     */
+    private var searchTimerTask: TimerTask? = null
 
     /**
      * When view is ready, load products
@@ -33,14 +44,11 @@ class ProductsViewModel @Inject constructor(
     private fun getProducts(userInitiateRefresh: Boolean = false, searchKeyword: String? = null) {
         _productLoadStates.postValue(ProductLoadStates.Loading)
         viewModelScope.launch(context = coroutineScope) {
-            val productItems =
-                productRepository.getAllProductsWithReFetchIfNeeded(
-                    forceInvalidateCatchAndReFetch = userInitiateRefresh,
-                    searchKeyword = searchKeyword
-                )
+            val productItems = productRepository.getAllProductsWithReFetchIfNeeded(
+                forceInvalidateCatchAndReFetch = userInitiateRefresh, searchKeyword = searchKeyword
+            )
             if (productItems.isEmpty().not()) {
                 _productLoadStates.postValue(ProductLoadStates.Loaded(productItems))
-
             } else {
                 _productLoadStates.postValue(ProductLoadStates.EmptyOrError("Failed to load or products not found"))
             }
@@ -51,12 +59,21 @@ class ProductsViewModel @Inject constructor(
         getProducts(true)
     }
 
-    /**
+    /*
+    **
      * Search products
      * If $searchKeyword is null, then it will search for all products
      */
     fun searchProducts(searchKeyword: String? = null) {
-        getProducts(false, searchKeyword)
-    }
 
+        searchTimerTask?.cancel()
+
+        // Schedule a delayed search operation
+        searchTimerTask = object : TimerTask() {
+            override fun run() {
+                getProducts(false, searchKeyword)
+            }
+        }
+        searchTimer.schedule(searchTimerTask, 500L)
+    }
 }
